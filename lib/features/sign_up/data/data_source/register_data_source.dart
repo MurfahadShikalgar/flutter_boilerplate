a4300b97058b014652b9e3eb3482dcdd80aa45c6
MurfahadShikalgar <murphy22dec@gmail.com>
2023-02-06 15:18:19 +0530

    new changes

--- lib/features/sign_up/data/data_source/register_data_source.dart
+++ lib/features/sign_up/data/data_source/register_data_source.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: avoid_print
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' ;
 
 Future<Response> getRegisterDataSource(Map body) async {
-  String url =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA5ISVU8sV3nnQYdnzkfcS0UspQFhvMQfk";
+  String url = dotenv.env['SIGNUP_API'].toString();
   Uri uri = Uri.parse(url);
   var bodyy = {"email": "aa.byteridge@gmail.com", "password": "Byteridge"};
   Response response = await post(uri, body: bodyy);
